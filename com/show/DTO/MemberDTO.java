8b9fe73cec817cdcbe943c7d8938ef0b1bc9ccf3
DESKTOP-73MOQ90\USER <177100174u@gmail.com>
2024-07-09 11:47:31 +0900

    V1.0.2

--- com/show/DTO/MemberDTO.java
+++ com/show/DTO/MemberDTO.java
@@ -6,42 +6,42 @@ public class MemberDTO {
 	// 필드
 	private String id;
 	private String pw;
-	private String name;
 	private String nickName;
 	private String birth;// 주민번호 앞자리 string으로 저장
 	private String sex; // 주민번호 뒷자리 첫번째 숫자 x%2이용하여 판별
 	private String pNo; // 전화번호 - 없이 저장
 	private Boolean loginStatus;
-	private boolean usability;
-	private Author author;
+	private Object author;
 
+	
 	// 생성자
-	public MemberDTO() {
-	      this.id = "guest";
-	      this.nickName = "guest";
-	      this.author = Author.GUEST;
-	      this.loginStatus=false;
-	}
-
-	public MemberDTO(String id, String pw) {// 로그인시 생성자
-		this.id = id;
-		this.pw = pw;
-		// this.nickName = LoginDTO. 로그인한 정보의 닉네임을 가져온다.
-		this.loginStatus = false; // 로그인 검증후 성공시 변경
-	}
-
-	public MemberDTO(String id, String pw, String ssn, String nickName, String pNo, String mail) {// 회원가입시 생성자
-		this.id = id;
-		this.pw = pw;
-		// this.birth = ssn에서 숫자추출하여 삽입(메서드)
-		// this.sex = ssn에서 숫자추출하여 삽입(메서드)
-		this.nickName = nickName;
-		this.pNo = pNo;
-		this.mail = mail;
-		this.author = Author.USER; // 회원가입시 권한부여
-		this.loginStatus = false; // 로그인하면 상태수정
-	}
-
+	  public MemberDTO() {
+//	      this.id = "guest";
+//	      this.nickName = "guest";
+//	      this.author = Author.GUEST;
+//	      this.loginStatus=false;
+	   }
+	   
+	   public MemberDTO(String id,String pw) {//로그인시 생성자
+	      this.id = id;
+	      this.pw = pw;      
+	  //    this.nickName = LoginDTO.   로그인한 정보의 닉네임을 가져온다.
+	      this.loginStatus=false; //로그인 검증후 성공시 변경
+	   }
+	   
+	   public MemberDTO(String id,String pw,String ssn,String nickName,String pNo,String mail) {//회원가입시 생성자
+	      this.id = id;
+	      this.pw = pw;
+	      //this.birth = ssn에서 숫자추출하여 삽입(메서드)
+	      //this.sex = ssn에서 숫자추출하여 삽입(메서드)
+	      this.nickName = nickName;
+	      this.pNo = pNo;
+	      this.mail = mail;
+	      this.author = Author.USER; //회원가입시 권한부여
+	      this.loginStatus=false; //로그인하면 상태수정
+	   }
+	
+	
 	// 메서드
 	public String getId() {
 		return id;
@@ -71,27 +71,8 @@ public class MemberDTO {
 		return mail;
 	}
 
-	public boolean setId(String joinID, ArrayList<MemberDTO> memberDTOs, boolean pass) {// 회원가입시 Id 생성 중복 검증용 setter	
-//		System.out.println("여기는 왓냐");			
-//		System.out.println(memberDTOs.size());
-			for (MemberDTO findId : memberDTOs) {
-				if (findId.getId().equals(joinID)) {					
-					this.usability = false;
-					System.out.println("사용중인 ID입니다.");
-					pass = false;
-					break;}
-								
-				}
-			this.id = joinID;
-			return pass;
-			
-			
-		
-
-	}
-
-	public void setUsability(boolean usability) {
-		this.usability = usability;
+	public void setId(String id,  ArrayList<MemberDTO> loginDTOs) { //회원가입시 Id  생성 중복 검증용 setter
+			this.id = id;
 	}
 
 	public void setPw(String pw) {
@@ -128,42 +109,14 @@ public class MemberDTO {
 
 	private String mail;
 
+
 	public Object getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(Author author) {
+	public void setAuthor(Object author) {
 		this.author = author;
 	}
-
 	
-
-	public boolean isUsability() {
-		return usability;
-	}
-
-	/*
-	 * public void setUsability(boolean usability) { this.usability = usability; }
-	 */
-
-	@Override
-	public String toString() {
-		return "MemberDTO [id=" + id + ", pw=" + pw + ", name=" + name + ", nickName=" + nickName + ", birth=" + birth
-				+ ", sex=" + sex + ", pNo=" + pNo + ", loginStatus=" + loginStatus + ", usability=" + usability
-				+ ", author=" + author + ", mail=" + mail + "]";
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setid(String string) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
--- com/show/MainExam.java
+++ com/show/MainExam.java
@@ -14,7 +14,7 @@ import com.show.SV.myPageSV;
 
 public class MainExam {
 
-	
+	public static MemberDTO lSt = new MemberDTO();
 	public static Scanner s = new Scanner(System.in);// 단어 입력
 	public static Scanner sL = new Scanner(System.in);// 긴 문장 입력
 	public static ArrayList<MemberDTO> loginDTOs = new ArrayList<MemberDTO>();
@@ -25,26 +25,14 @@ public class MainExam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 MemberDTO guest = new MemberDTO();
-		 loginState=guest;
-	     
-	     MemberDTO guest1 = new MemberDTO();
-	     guest1.setName("0");
-	     guest1.setid("0");
-	     guest1.setPw("0");
-	     loginDTOs.add(guest1);
-	     
-//	     MemberDTO guest2 = new MemberDTO();
-//	     guest2.setName("3");
-//	     guest2.setid("3");
-//	     guest2.setPw("3");
-//	     loginDTOs.add(guest2);
+	     loginState=guest;
 		
-		loginState.setLoginStatus(false);				
+		lSt.setLoginStatus(false);				
 		boolean run = true;
 		System.out.println("<오잼>(가제 : 오늘은 무엇이 재밌을까?)에 오신것을 환영합니다.");
 	
 		while (run) {
-			if (loginState.getLoginStatus()) {
+			if (lSt.getLoginStatus()) {
 				System.out.println("아래의 메뉴에서 골라주세요.");
 				System.out.println("1. 찾아보기  |  2. 내정보확인");
 				System.out.print(">>>");
@@ -53,11 +41,11 @@ public class MainExam {
 				switch(select) {
 				case "1" :
 					System.out.println("검색메뉴로 진입합니다.");
-					FIndSV.menu(loginState, s, sL, loginDTOs, reviewDTOs, showDTOs);
+					FIndSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
 					break;
 				case "2" :
 					System.out.println("내정보확인 메뉴로 진입합니다.");
-					loginState = myPageSV.menu(loginState, s, sL, loginDTOs, reviewDTOs, showDTOs);
+					lSt = myPageSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
 					break;
 				default :
 					System.out.println("1~2사이에서 입력해주시기 바랍니다.");
@@ -71,11 +59,11 @@ public class MainExam {
 				switch(select) {
 				case "1" :
 					System.out.println("검색메뉴로 진입합니다.");
-					FIndSV.menu(loginState, s, sL, loginDTOs, reviewDTOs, showDTOs);
+					FIndSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
 					break;
 				case "2" :
 					System.out.println("로그인 메뉴로 진입합니다.");
-					loginState = LoginSV.menu(loginState, s, sL, loginDTOs, reviewDTOs, showDTOs);
+					lSt = LoginSV.menu(lSt, s, sL, loginDTOs, reviewDTOs, showDTOs);
 					break;
 				default :
 					System.out.println("1~2사이에서 입력해주시기 바랍니다.");
